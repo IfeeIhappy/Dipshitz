1181bcc27f767b3ec981b5e562ff58f405e9a27f
Glitch (first-end) <none>
2020-12-13 16:33:56 +0000

    🍈🦄 Checkpoint ./package.json:13536473/1436 ./server.js:13536473/388

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("src"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/src/index.html");
+  response.sendFile("/index/");
 });
 
 // send the default array of dreams to the webpage
