00ba84ebd0f7e2058b09aedc305595366d3feffb
Glitch (first-end) <none>
2020-12-26 16:09:05 +0000

    🏹🔖 Checkpoint ./server.js:13536473/944 ./package.json:13536473/20

--- server.js
+++ server.js
@@ -1,38 +1,24 @@
 const fs = require('fs');
 const express = require('express');
 const showdown  = require('showdown');
-const converter = new showdown.Converter();
 
 const app = express();
 const port = 3000;
-const path = require('./posts'); 
+const path = './posts';
 
 app.set('view engine', 'pug');
 
 app.get('/read/:post', (req, res) => {
   if (typeof(req.params.post) !== "undefined") {
-    const filenames = fs.readdirSync(path, function(err, files){
-      files = files.map(function (fileName) {
-      return {
-        name: fileName,
-        time: fs.statSync(path + '/' + fileName).mtime.getTime()
-      };
-      })
-      .sort(function (a, b) {
-        return a.time - b.time; })
-      .map(function (v) {
-        return v.name; });
-    });
     var posts = [];
-    filenames.forEach(file => { 
-      if (path.extname(file) == ".md") {
-        posts.push(file.split('.')[0]);
-      }
+    fs.readdirSync(path).forEach(file => {
+      posts.push(file);
     });
-    if (posts.indexOf(req.params.post) > -1){
-      const markdown = fs.readfileSync(`./posts/${req.params.post}.md`);
-      const html = converter.makeHtml(markdown);
-      const title = markdown.match(/(\w.*)\n/)[0];
+    if (posts.indexOf(`${req.params.post}.md`) > -1){
+      const text = fs.readFileSync(`./posts/${req.params.post}.md`);
+      const converter = new showdown.Converter();
+      const html = converter.makeHtml(text);
+      const title = text.match(/(\w.*)\n/)[0];
       res.render('default', { title: title, content: html });
     } else {
       res.redirect('/');
