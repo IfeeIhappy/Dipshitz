16ea43d31a7850b9e0d225727287f8d5d8cab0ca
Glitch (first-end) <none>
2020-12-26 15:44:39 +0000

    💝🎃 Checkpoint ./server.js:13536473/1474

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = 'posts';
 
 app.set('view engine', 'pug');
 
-app.get('/:post', (req, res) => {
-  if (req.params.post == "") {
-    res.render('default', { title: 'Post', content: req
-                           .params.post });
+app.get('/read/:post', (req, res) => {
+  if (typeof(req.params.post) !== "undefined") {
+    const path = require(path); 
+    const files = fs.readdirSync(__dirname); 
+    var posts = [];
+    files.forEach(file => { 
+      if (path.extname(file) == ".md") {
+        posts.push(file.split('.')[0]);
+      }
+    });
+    if (posts.indexOf(req.params.post) > -1){
+      res.render('default', { title: 'Post', content: req.params.post });
+    } else {
+      res.redirect('/');
+    }
   } else {
-    res.render('default', { title: 'Hey', content: 'Hello there!' });
+    res.redirect('/');
   }
+});
+
+app.get('/', (red, res) => {
+   res.render('default', { title: 'Hey', content: 'Hello there!' });
 });
 
 app.listen(port, () => {
