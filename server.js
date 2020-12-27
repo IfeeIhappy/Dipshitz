e4b33aae5b4ea248136e6b1c5883fcc4e1adfcb8
Glitch (first-end) <none>
2020-12-27 11:13:34 +0000

    💃📌 Checkpoint ./css/style.css:13536473/1902 ./posts/how-it-works.md:13536473/23 ./views/footer.pug:13536473/270 ./.gitignore:13536473/15 ./drafts/index.md:13536473/17 ./server.js:13536473/3956

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = "./posts";
 app.use(express.json());
 app.set("view engine", "pug");
 
+/* When a user follows a direct link to a published post */
 app.get("/read/:post", (req, res) => {
   if (typeof req.params.post !== "undefined") {
     var posts = [];
@@ -29,30 +30,35 @@ app.get("/read/:post", (req, res) => {
   }
 });
 
+/* List all published posts on the index page */
 app.get("/", (req, res) => {
   const text = fs.readFileSync("./drafts/index.md", "utf8");
   const html = converter.makeHtml(text);
   const title = text.match(/(\w.*)\n/)[0];
   var list = fs.readdirSync(path).map(file => {
-      var filename = file.split('.')[0];
-      var title = filename.replace(/\-/gi," ");
-      return `<li><a href='/read/${filename}'>${title}</a></li>`;
-    });
+    var filename = file.split(".")[0];
+    var title = filename.replace(/\-/gi, " ");
+    return `<li><a href='/read/${filename}'>${title}</a></li>`;
+  });
   res.render("index", { title: title, content: html, list: list });
 });
 
+/* Create new posts via API */
 app.post("/write", (req, res) => {
-  if(req.body.key == process.env.key){
+  if (req.body.key == process.env.key) {
     res.sendStatus(200);
-    fs.writeFile(`./posts/${req.body.title}.md`, req.body.content, function (err) {
+    fs.writeFile(`./posts/${req.body.title}.md`, req.body.content, function(
+      err
+    ) {
       if (err) return console.log(err);
-      console.log('Wrote file', req.body.title);
+      console.log("Wrote file", req.body.title);
     });
   } else {
-    req.send('Unauthorized');
+    req.send("Unauthorized");
   }
 });
 
+/* Start listening */
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at port ${port}`);
 });
